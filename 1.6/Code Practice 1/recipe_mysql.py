fc3fac6340981b06ff487c79d781a5a0974acd4c
Alex Sweeney <ajsweeney2324@gmail.com>
2024-09-24 21:32:05 -0500

    create recipe

--- 1.6/Code Practice 1/recipe_mysql.py
+++ 1.6/Code Practice 1/recipe_mysql.py
@@ -18,8 +18,38 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS Recipes (
                difficulty       VARCHAR(20)    
 );''')
 
+
 def create_recipe(conn, cursor):
-    return 1
+    name = str(input("What would you like to call this recipe? "))
+    cooking_time = int(input("Total Cook + Prep time: "))
+    ingredients = list(input("Please list the required ingredients, separated by commas: ").split(", "))
+    
+    def calculate_difficulty(cooking_time, ingredients):
+        numberOfIngredients = len(ingredients)
+        if cooking_time < 10:
+            if numberOfIngredients < 4:
+                return "Easy"
+            else:
+                return "Medium"
+        else:
+            if numberOfIngredients < 4:
+                return "Intermediate"
+            else:
+                return "Hard" 
+
+    difficulty = calculate_difficulty(cooking_time, ingredients)        
+    returned_string = ', '.join(ingredient.strip() for ingredient in ingredients)
+
+    query = """
+    INSERT INTO recipes (name, cooking_time, ingredients, difficulty)
+    VALUES (?, ?, ?, ?)
+    """
+    
+    cursor.execute(query, (name, cooking_time, returned_string, difficulty))
+    conn.commit()
+
+    print(f"Recipe '{name}' has been added to the database!")
+
 
 def search_recipe(conn, cursor):
     return 1
@@ -58,3 +88,6 @@ def main_menu(conn, cursor):
             break
         else:
             print("Invalid choice, please try again.")
+
+
+
